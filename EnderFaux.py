4e28a12d1be5aeb02318311b012e463f47fa584e
Ayush Verma <av874559@gmail.com>
2025-08-03 15:07:38 +0530

    Initial Commit

---
+++ EnderFaux.py
@@ -0,0 +1,23 @@
+# Importing the required module
+import random
+
+# Creating data for lists
+subjects = [
+    "Priyanka Chopra",
+    "Virat Kohli",
+    "Narendra Modi",
+    "Nitin Gadkare",
+    "Nirmala Sitaraman",
+    "A group of bikers",
+    "Students of IIT Bombay",
+    "People of Maharashtra",
+    "A big horned Cow",
+    "A woman in scooty",
+    "Famous Youtuber"
+]
+Actions = [
+    
+]
+Objects = [
+    
+]
